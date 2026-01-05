748d1cbb011ae84061a5a05f6198c56363178c95
rziyoda2197 <rziyoda2197@gmail.com>
2026-01-05 12:32:50 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from royxat import *
+
+royhat = [12, -5, 8, 7, -3, 15, 20, -7]
+
+
+juft_sonlar = juft_topish(royhat)
+print("Ro'yhatdagi juft sonlar:", juft_sonlar)
+
+
+toq_son = toq_topish(royhat)
+print("Ro'yhatdagi toq sonlar:", toq_son)
+
+
+
+musbat_top = musbat_topish(royhat)
+print("Ro'yhatdagi toq sonlar:", musbat_top)
+
